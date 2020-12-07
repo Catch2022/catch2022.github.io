230ec66e992a0f6b280e9f1dcfd9ff22885ea3c8
martinfalke <mafalke@kth.se>
2020-12-08 00:35:38 +0100

    Bug fixes to responsiveness, navbar, restored some structure. Added some color and changed some. Added some more icons and made additional styling changes.

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,23 +2,25 @@
 var titleHidden = false;
 var onceFlag = true; // only used for testing where hot reload leaves the page at the bottom,
                      // meaning titleHidden is false and won't be changed until the page scrolls to the top
-$(function() {
-  $(window).scroll(function() {
-    var x = $(window).scrollTop();
-    var vh = $(window).height();
-    if (x <= vh*0.55) {
-      if(!titleHidden){
-        $("#navbar-title").animate({opacity: 0.0}, 300);
-        $("#main-nav").animate({padding: "8px 0"}, 300);
-        titleHidden = true;
-      }
-    } else {
-      if(titleHidden || onceFlag){
-        $("#navbar-title").animate({opacity: 1.0}, 300);
-        $("#main-nav").animate({padding: "24px 0"}, 300);
-        titleHidden = false;
-        onceFlag = false;
-      }
+function navbarOnScroll() {
+  var x = $(window).scrollTop();
+  var vh = $(window).height();
+  if (x <= vh*0.55) {
+    if(!titleHidden){
+      $("#navbar-title").animate({opacity: 0.0}, 300);
+      $("#main-nav").animate({padding: "8px 0"}, 300);
+      titleHidden = true;
     }
-  });
+  } else {
+    if(titleHidden || onceFlag){
+      $("#navbar-title").animate({opacity: 1.0}, 300);
+      $("#main-nav").animate({padding: "24px 0"}, 300);
+      titleHidden = false;
+      onceFlag = false;
+    }
+  }
+}
+navbarOnScroll(); // run once to make sure it is correct on reload with a #link
+$(function() {
+  $(window).scroll(navbarOnScroll);
 });
