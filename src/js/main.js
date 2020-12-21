4d8bcc5e7ced35c542ec754f263e645c9178e621
martinfalke <mafalke@kth.se>
2020-12-21 23:45:17 +0100

    Second last edition to website, only missing an added GDD link/removal of its div

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-// adjust the width so that there is never horizontal overflow
-function setVw() {
-    let vw = document.documentElement.clientWidth / 100;
-    document.documentElement.style.setProperty('--vw', `${vw}px`);
-  }
-setVw();
-window.addEventListener('resize', setVw);
-
-
